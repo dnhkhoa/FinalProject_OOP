a86e29669d743ea844ec295592c877373b03a611
KhanzzOnlySp <khanganduc@gmail.com>
2024-11-21 22:07:13 +0700

    a

---
+++ FinalProject_OOP/UC_AddItems1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Drawing;
+
+namespace FinalProject_OOP
+{
+    internal class UC_AddItems
+    {
+        public UC_AddItems()
+        {
+        }
+
+        public bool Visible { get; internal set; }
+        public Point Location { get; internal set; }
+        public string Name { get; internal set; }
+        public Size Size { get; internal set; }
+        public int TabIndex { get; internal set; }
+
+        internal void BringToFront()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
