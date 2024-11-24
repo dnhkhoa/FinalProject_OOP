3e3b13a503fa15180c6d2ef0c0ef82c1df954702
dnhkhoa <truongdinhkhoa3751@gmail.com>
2024-11-25 00:36:32 +0700

    data

--- FinalProject_OOP/Login.cs
+++ FinalProject_OOP/Login.cs
@@ -13,7 +13,7 @@ namespace FinalProject_OOP
 {
     public partial class frmLogin : Form
     {
-        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ASUS-PC\Documents\Cafe.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\DELL\\Documents\\CoffeeManagement.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
         
         private void frmLogin_Load(object sender, EventArgs e)
         {
